bd3219d8f182ba153fb75418a580ad08ab71abce
Jaclyn Hardy <jackihardy@yahoo.com>
2020-07-17 12:00:51 -0700

    updated style

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,13 @@
- 
+$(".card-title").on("click", function(){
+    var id = $(this).attr("data-id")
+    $.ajax({
+        url: "http://theoatmeal.com/blog" +id,
+        method: "GET"
+    }).then(function(data){
+        location.reload()
+    })
+})
+
 $(".saveButton").on("click", function(){
     var id = $(this).attr("data-id")
     $.ajax({
